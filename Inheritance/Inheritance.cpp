84945a2888f21fe17d6d48ef842179eb4dbdf510
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-05-12 19:40:09 +0700

    deklarasi dan pembuatan class orang

--- Inheritance/Inheritance.cpp
+++ Inheritance/Inheritance.cpp
@@ -1,3 +1,26 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+//pembuatan class orang
+class orang {
+public:
+    //deklarasi member variabel dari class orang
+    string nama;
+
+    //pembuatan constructor dari class orang
+    orang(string pNama) :
+    nama (pNama) {
+        cout << "orang dibuat\n" << endl;
+    }
+
+    //pembuatan destructor dari class orang
+    ~orang() {
+        cout << "orang dihapus\n" << endl;
+    }
+
+    //pembuatan function jumlah
+    int jumlah(int a, int b) {
+        return a + b;
+    }
+};
