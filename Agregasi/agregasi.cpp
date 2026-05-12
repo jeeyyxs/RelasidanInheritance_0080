20dae6c871143a839ded5d326eff525b12b21654
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-05-12 18:56:40 +0700

    pemanggilan dan pemberian argument pada prosedur tambahAnak

--- Agregasi/agregasi.cpp
+++ Agregasi/agregasi.cpp
@@ -12,3 +12,8 @@ int main()
     anak* varAnak1 = new anak("tono");
     anak* varAnak2 = new anak("tiara");
     anak* varAnak3 = new anak("dini");
+
+    //pemanggilan dan pemberian argument pada prosedur tambahAnak()
+    varibu->tambahAnak(varAnak1);
+    varibu->tambahAnak(varAnak2);
+    varibu2->tambahAnak(varAnak3);
