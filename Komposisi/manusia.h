6d9ec9ed693f781e2c62ab2fdc78f7722ba49a68
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-05-12 20:08:02 +0700

    membuat deklarasi dan class manusia

---
+++ Komposisi/manusia.h
@@ -0,0 +1,26 @@
+#ifndef MANUSIA_H
+#define MANUSIA_H
+#include <iostream>
+#include <string>
+#include "jantung.h"
+using namespace std;
+
+//membuat class manusia di file header manusia.h
+class Manusia {
+public:
+    //membuat member vartabel class manusia
+    string name;
+    Jantung varJantung;
+
+    //membuat constructor dan destructor class manusia
+    Manusia(string pName) : name(pName) {
+        cout << name << "Hidup\n";
+    }
+
+    //membuat destructor class manusia
+    ~Manusia() {
+        cout << name << "Mati\n";
+    }
+};
+
+#endif // !MANUSIA_H
