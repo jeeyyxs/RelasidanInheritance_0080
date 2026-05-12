25cde118ba6093716d70176a637c9f432ce94788
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-05-12 17:57:33 +0700

    membuat destructor pada class anak

--- Agregasi/anak.h
+++ Agregasi/anak.h
@@ -9,3 +9,9 @@ public:
     anak(string pNama) :nama(pNama) {
         cout << "Anak \"" << nama << "\" ada\n";
     }
+    ~anak() {
+    cout << "Anak \"" << nama << "\" tidak ada\n";
+    }
+};
+
+#endif
