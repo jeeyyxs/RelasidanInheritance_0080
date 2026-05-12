9a6f828bc2c27d540226a07ac4824f9fccc8c500
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-05-12 17:54:32 +0700

    menambahkan constructor

--- Agregasi/anak.h
+++ Agregasi/anak.h
@@ -2,3 +2,10 @@
 using namespace std;
 #ifndef ANAK_H
 #define ANAK_H
+
+class anak {
+public:
+    string nama;
+    anak(string pNama) :nama(pNama) {
+        cout << "Anak \"" << nama << "\" ada\n";
+    }
