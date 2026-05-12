dfccc67b013529c9bb92882ae535119f261530ee
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-05-12 18:52:44 +0700

    deklarasi dan pemberian nilai variabel pointer untuk objek

--- Agregasi/agregasi.cpp
+++ Agregasi/agregasi.cpp
@@ -3,3 +3,12 @@ using namespace std;
 
 #include "anak.h"
 #include "ibu.h"
+
+int main()
+{
+    //deklarasi dan pemberian nilai variabel pointer untuk objek dari class ibu dan anak
+    ibu* varibu = new ibu("rani");
+    ibu* varibu2 = new ibu("sari");
+    anak* varAnak1 = new anak("tono");
+    anak* varAnak2 = new anak("tiara");
+    anak* varAnak3 = new anak("dini");
